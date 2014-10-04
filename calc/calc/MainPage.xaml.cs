e00e8ee0a6e9e3c4123837fe2f7f93b13d0758af
Adam Michałowski <adasm@live.com>
2014-10-04 18:12:46 +0200

    Calc finish

--- calc/calc/MainPage.xaml.cs
+++ calc/calc/MainPage.xaml.cs
@@ -13,36 +13,142 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
-
 namespace calc
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+ 
         }
+
+        float result = 0, digitMul = 1;
+        float   lastNumber = 0, currentNumber = 0;
+        char  currentOperand = '#';
+        bool  dot = false;
+        
+
+        private void PrintResult()
+        {
+            Result.Text = result.ToString();
+        }
+
+        private void PrintCurrent()
+        {
+            Result.Text = currentNumber.ToString();
+        }
+
+        private void Process()
+        {
+            float x = currentNumber > 0 ? currentNumber : lastNumber;
+
+            switch (currentOperand)
+            {
+                case '+': result += x; break;
+                case '-': result -= x; break;
+                case '×': result *= x; break;
+                case '÷': result /= x; break;
+                default: result = currentNumber; break;
+            }
+            
+            if (currentNumber > 0)
+            {
+                lastNumber = currentNumber;
+                ClearCurrent();
+            }
+            
+
+            PrintResult();
+        }
+
+        private void Operand(char op)
+        {
+            if (currentNumber > 0)
+            {
+                if (currentOperand != '#')
+                    Process();
+                else
+                    result = currentNumber;
+            }
+
+            ClearCurrent();
+            currentOperand = op;
+        }
+
+        private void ClearCurrent()
+        {
+            currentNumber = 0;
+            digitMul = 1;
+            dot = false;
+        }
+
+        private void Clear()
+        {
+            result = 0;
+            lastNumber = 0;
+            ClearCurrent();
+            currentOperand = '#';
+            dot = false;
+            PrintResult();
+        }
+
+        private void Button_Click_Digit(object sender, RoutedEventArgs e)
+        {
+            int digit = (((Button)sender).Content.ToString()[0] - '0');
+            digitMul = (dot ? digitMul / 10 : 1);
+            currentNumber = (dot ? 1 : 10) * currentNumber + digit * digitMul;
+            PrintCurrent();
+        }
+
+        private void Button_Click_Op(object sender, RoutedEventArgs e)
+        {
+            char op = (((Button)sender).Content.ToString()[0]);
+            dot = false;
+            switch (op)
+            {
+                case '+': Operand(op); break;
+                case '-': Operand(op); break;
+                case '×': Operand(op); break;
+                case '÷': Operand(op); break;
+                case '=': Process(); break;
+                case 'A': Clear(); break;
+                case ',': dot = true; break;
+            }
+        }
+
+        SolidColorBrush[] brushes = {
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 250, 50, 50)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 50, 250, 50)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 50, 50, 250)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 250, 50, 250)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 250, 250, 50)),
+            new SolidColorBrush(Windows.UI.Color.FromArgb(255, 50, 250, 250))
+        };
+        int currentBrush = 0;
+
+        private void Button_Click_CC(object sender, RoutedEventArgs e)
+        {
+            if(++currentBrush > brushes.Length - 1)
+                currentBrush = 0;
+            Result.Foreground = brushes[currentBrush];
+        }
+
+        private void Button_Click_UP(object sender, RoutedEventArgs e)
+        {
+            Result.FontSize += 2;
+        }
+
+        private void Button_Click_DOWN(object sender, RoutedEventArgs e)
+        {
+            Result.FontSize -= 2;
+        }
+
     }
 }
